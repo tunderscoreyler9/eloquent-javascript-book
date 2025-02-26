e6cc110432c033f41305623f893a59480b0d1a71
Tyler <tyler.s9@proton.me>
2025-02-25 20:34:41 -0500

    Added findSolution function

--- chapter_3/recursion.js
+++ chapter_3/recursion.js
@@ -15,3 +15,21 @@ const recursivePower = (base, exponent) => {
         return base * recursivePower(base, exponent - 1);
     }
 };
+
+
+// Write a function named findSolution that, given a target number, determines a sequence 
+// of operations that starts from the number 1 and, through a combination of additions and 
+// multiplications, arrives at the target number.
+const findSolution = target => {
+    function find (current, history) {
+        if (current == target) {
+            return history;
+        } else if (current > target) {
+            return null;
+        } else {
+            return find(current + 5, `(${history} + 5)`) ?? 
+            find (current * 3, `(${history} * 3)`);
+        }
+    }
+    return find(1, "1");
+};
