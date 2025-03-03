3b0e000d223c4fc17619d2c59119707321d72e24
Tyler Skidmore <tyler.s9@proton.me>
2025-03-03 12:32:00 -0500

    Added recursive Fibonacci function

--- chapter_3/fibonacci.js
+++ chapter_3/fibonacci.js
@@ -1,10 +1,24 @@
-function fibonacci() {
-    let fib = [0, 1];
-    let n = Number(prompt("Enter a # for the desired Nth Fibonacci.."))
 
-    for (let i = 2; i <= n; i++) {
-        fib[i] = fib[i - 2] + fib[i - 1];
-    };
+// Iterative approach:
+// function fibonacci() {
+//     let fib = [0, 1];
+//     let n = Number(prompt("Enter a # for the desired Nth Fibonacci.."))
 
-    return `F_sub_${n} = ${fib[n]}`;
+//     for (let i = 2; i <= n; i++) {
+//         fib[i] = fib[i - 2] + fib[i - 1];
+//     };
+
+//     return `F_sub_${n} = ${fib[n]}`;
+// };
+
+// Recrusive approach:
+const recursiveFibonacci = (n) => {
+    if (n === 0) {
+        return 0;
+    }
+    if (n === 1) {
+        return 1;
+    }
+    return recursiveFibonacci(n - 1) + recursiveFibonacci(n - 2);
+
 };
